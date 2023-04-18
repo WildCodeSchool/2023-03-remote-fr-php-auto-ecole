8d4506a3e287f6988144f9f12bc016112a71cbe2
jfm <jeanfrancois.morin@wildcodeschool.com>
2023-04-18 14:46:29 +0200

    livecoding rapide add new training

--- src/Controller/TrainingController.php
+++ src/Controller/TrainingController.php
@@ -9,6 +9,23 @@ class TrainingController extends AbstractController
 {
     public function index()
     {
+        $errors = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $training = array_map('trim', $_POST);
+
+            // TODO validations (length, format...)
+            if (empty($training['title'])) {
+                $errors[] = "Le champ titre est obligatoire";
+            }
+
+            if (empty($errors)) {
+                $trainingManager = new TrainingManager();
+                $trainingManager->insert($training);
+                header('Location: /training');
+                return null;
+            }
+        }
         $trainingManager = new TrainingManager();
         $trainings = $trainingManager->selectAll();
         $licenceManager = new LicenceManager();
--- src/Model/TrainingManager.php
+++ src/Model/TrainingManager.php
@@ -5,4 +5,15 @@ namespace App\Model;
 class TrainingManager extends AbstractManager
 {
     public const TABLE = 'training';
+
+    public function insert(array $training): int
+    {
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`title`, `content`) 
+        VALUES (:title, :content)");
+        $statement->bindValue('title', $training['title'], \PDO::PARAM_STR);
+        $statement->bindValue('content', $training['content'], \PDO::PARAM_STR);
+
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
 }
